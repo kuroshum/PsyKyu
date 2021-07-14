0a5a95b86c8923fa5bc825fda50d1d87ad95d66b
chihhar <s236206@wakayama-u.ac.jp>
2021-07-14 13:28:59 +0900

    サンプル：NavMesh,StateMachine

---
+++ Assets/Script/EnemyMeshController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyMeshController : MonoBehaviour
+{
+    public GameObject TargetObject;//目標位置
+    NavMeshAgent m_navMeshAgent; /// NavMeshAgent
+    
+    
+    void Start()
+    {
+        // NavMeshAgentコンポーネントを取得
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // NavMeshが準備できているなら
+        if (m_navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            // NavMeshAgentに目的地をセット
+            m_navMeshAgent.SetDestination(TargetObject.transform.position);
+        }
+    }
+}
